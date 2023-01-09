de2c1d063c356786c36e9ecdd108965671501418
Andrey Zbukar <andrey.zbk@gmail.com>
2023-01-09 17:31:42 +0200

    Initial implementation. Creates separate pages for each LB component.

---
+++ src/Plugin/migrate/process/YLBSection.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\y_lb_demo_content\Plugin\migrate\process;
+
+use Drupal\layout_builder\Section;
+use Drupal\layout_builder\SectionComponent;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Fills in Layout Builder sections.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "lb_sections"
+ * )
+ */
+class YLBSection extends ProcessPluginBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    if (!$value) {
+      return NULL;
+    }
+
+    $sections = [];
+
+    foreach ( $value as $section ) {
+      $components = [];
+      $layout_id = "bootstrap_layout_builder:{$section['bs_col']}";
+      $layout_settings = [
+        "label" => $section['label'] ?? '',
+        "container_wrapper_classes" => $section['container_wrapper_classes'] ?? '',
+        "container_wrapper_attributes" => $section['container_wrapper_attributes'] ?? NULL,
+        "container_wrapper" => $section['container_wrapper'] ?? [],
+        "container_wrapper_bg_color_class" => $section['container_wrapper_bg_color_class'] ?? '',
+        "container_wrapper_bg_media" => $section['container_wrapper_bg_media'] ?? NULL,
+        "container" => $section['container'] ?? '',
+        "section_classes" => $section['section_classes'] ?? '',
+        "section_attributes" => $section['section_attributes'] ?? NULL,
+        "regions_classes" => $section['regions_classes'] ?? [],
+        "regions_attributes" => $section['regions_attributes'] ?? [],
+        "breakpoints" => $section['breakpoints'] ?? [],
+        "layout_regions_classes" => $section['layout_regions_classes'] ?? [],
+        "context_mapping" => $section['context_mapping'] ?? [],
+        "remove_gutters" => $section['remove_gutters'] ?? "0",
+      ];
+
+      if (!empty($section['components'])) {
+        foreach ($section['components'] as $key => $componentConfig) {
+          $component = new SectionComponent($componentConfig['uuid'], $componentConfig['region'], $componentConfig['config'], $additional = []);
+          $components[$componentConfig['uuid']] = $component;
+        }
+      }
+      $section = new Section($layout_id, $layout_settings, $components, $third_party_settings = []);
+      $sections[] = $section;
+    }
+    return $sections;
+  }
+}
