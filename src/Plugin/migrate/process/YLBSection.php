afdd54eda4f2db286d2664d8a0c1b9f6c80d521f
Alexey Aleev <alexey.aleev@fivejars.com>
2023-09-28 14:33:26 +0200

    [DS-1042][DS-1097] Add a new demo homepage for Y LB components

--- src/Plugin/migrate/process/YLBSection.php
+++ src/Plugin/migrate/process/YLBSection.php
@@ -57,7 +57,12 @@ class YLBSection extends ProcessPluginBase implements ContainerFactoryPluginInte
     if (!$value) {
       return NULL;
     }
-
+//    $third_party_settings['y_lb']['override_styles'] = 1;
+//    $third_party_settings['y_lb']['styles'] = [
+//      'colorway' => 'ws_colorway_blue',
+//      'border_radius' => 'ws_border_radius_20',
+//      'border_style_global' => 'ws_border_style_global_drop_shadow',
+//    ];
     $sections = [];
     foreach ( $value as $section ) {
       $components = [];
@@ -83,6 +88,7 @@ class YLBSection extends ProcessPluginBase implements ContainerFactoryPluginInte
       if (!empty($section['components'])) {
         foreach ($section['components'] as $key => $componentConfig) {
           $config = $componentConfig['config'];
+          $additional = $componentConfig['additional'] ?? [];
           if(empty($componentConfig['block_config'])) {
             $blocks = $this->blockContentStorage->loadByProperties(['uuid' => $componentConfig['uuid']]);
             if (!$blocks) {
@@ -94,7 +100,7 @@ class YLBSection extends ProcessPluginBase implements ContainerFactoryPluginInte
           }
 
           $uuid = $componentConfig['uuid'] ?? $this->uuidService->generate();
-          $component = new SectionComponent($uuid, $componentConfig['region'], $config , $additional = []);
+          $component = new SectionComponent($uuid, $componentConfig['region'], $config , $additional);
           $components[$uuid] = $component;
         }
       }
